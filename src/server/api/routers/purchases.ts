04e0b75a7edc26b565b9f9c12f810dd7060b0bd2
Lucas Travessa <lucas.cos.travessa@gmail.com>
2023-11-23 14:34:40 -0300

    WIP: sales route

--- src/server/api/routers/purchases.ts
+++ src/server/api/routers/purchases.ts
@@ -1,6 +1,14 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const purchaseSchema = z.object({
+  id: z.number(),
+  supplier_id: z.number(),
+  total: z.number(),
+  date: z.date(),
+  status: z.string(),
+});
+
 export const purchasesRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.db.purchases.findMany();
@@ -9,47 +17,27 @@ export const purchasesRouter = createTRPCRouter({
     return ctx.db.purchases.findUnique({ where: { id: input } });
   }),
   create: publicProcedure
-    .input(
-      z.object({
-        supplier_id: z.number(),
-        total: z.number(),
-        date: z.date(),
-        status: z.string(),
-      }),
-    )
+    .input(purchaseSchema.omit({ id: true }))
     .mutation(({ ctx, input }) => {
       return ctx.db.purchases.create({
         data: {
-            supplier_id: input.supplier_id,
-            total: input.total,
-            date: input.date,
-            status: input.status,
-          // createdBy: { connect: { id: ctx.session.user.id } },
+          supplier_id: input.supplier_id,
+          total: input.total,
+          date: input.date,
+          status: input.status,
         },
       });
     }),
   update: publicProcedure
-    .input(
-      z
-        .object({
-            id: z.number(),
-            supplier_id: z.number(),
-            total: z.number(),
-            date: z.date(),
-            status: z.string(),
-        })
-        .partial()
-        .required({ id: true }),
-    )
+    .input(purchaseSchema.partial().required({ id: true }))
     .mutation(({ ctx, input }) => {
       return ctx.db.purchases.update({
         where: { id: input.id },
         data: {
-            supplier_id: input.supplier_id,
-            total: input.total,
-            date: input.date,
-            status: input.status,
-          // createdBy: { connect: { id: ctx.session.user.id } },
+          supplier_id: input.supplier_id,
+          total: input.total,
+          date: input.date,
+          status: input.status,
         },
       });
     }),
--- src/server/api/routers/sales.ts
+++ src/server/api/routers/sales.ts
@@ -1,6 +1,24 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const salesDetailSchema = z.object({
+  id: z.number(),
+  products_id: z.number(),
+  products_amount: z.number(),
+  price: z.number(),
+  sales_id: z.number(),
+});
+
+const salesSchema = z.object({
+  id: z.number(),
+  client: z.string(),
+  total: z.number(),
+  date: z.date(),
+  employee_id: z.number(),
+  sales_details_id: z.array(z.number()),
+  sales_details: z.array(salesDetailSchema),
+});
+
 export const salesRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.db.sales.findMany();
@@ -9,37 +27,30 @@ export const salesRouter = createTRPCRouter({
     return ctx.db.sales.findUnique({ where: { id: input } });
   }),
   create: publicProcedure
-    .input(
-      z.object({
-        client: z.string(),
-        total: z.number(), 
-        date: z.date(),
-        employee_id: z.number(),
-      }),
-    )
+    .input(salesSchema.omit({ id: true }))
     .mutation(({ ctx, input }) => {
+      input.sales_details.map((detail) => {
+        ctx.db.salesDetails.create({
+          data: {
+            products_id: detail.products_id,
+            products_amount: detail.products_amount,
+            price: detail.price,
+            sales_id: input.id,
+          },
+        });
+      });
       return ctx.db.sales.create({
         data: {
           client: input.client,
           total: input.total,
           date: input.date,
-          employee_id: input.employee_id, 
-          // createdBy: { connect: { id: ctx.session.user.id } },
+          employee_id: input.employee_id,
+          sales_details_id: input.sales_details_id,
         },
       });
     }),
   update: publicProcedure
-    .input(
-      z
-        .object({
-          id: z.number(),
-          client: z.string(),
-          total: z.number(), 
-          date: z.date(),
-        })
-        .partial()
-        .required({ id: true }),
-    )
+    .input(salesSchema.partial().required({ id: true }))
     .mutation(({ ctx, input }) => {
       return ctx.db.sales.update({
         where: { id: input.id },
@@ -47,6 +58,7 @@ export const salesRouter = createTRPCRouter({
           client: input.client,
           total: input.total,
           date: input.date,
+          employee_id: input.employee_id,
           // createdBy: { connect: { id: ctx.session.user.id } },
         },
       });
