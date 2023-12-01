88a84714a1b1e943ce2b2f9d6043cb8820daa6e6
Lucas Travessa <lucas.cos.travessa@gmail.com>
2023-12-01 08:57:24 -0300

    WIP: Sales Modal

--- src/app/(application)/(pages)/vendas/page.tsx
+++ src/app/(application)/(pages)/vendas/page.tsx
@@ -1,6 +1,7 @@
 import ModalGlobal from "~/app/_components/Modals/Modal";
 import SellsTable from "./table";
 import { api } from "~/trpc/server";
+import SalesForm from "~/app/_components/Form/SalesForm/SalesForm";
 
 export default async function Vendas() {
   const sells = await api.sales.getAll.query();
@@ -10,7 +11,7 @@ export default async function Vendas() {
       <SellsTable sells={sells} />
       <ModalGlobal>
         <h1 className="text-2xl font-bold">Adicione uma nova venda:</h1>
-        
+        <SalesForm />
       </ModalGlobal>
     </>
   );
---
+++ src/app/_components/Form/SalesForm/SalesForm.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { Button, Input } from "@nextui-org/react";
+import { useSales } from "./useSales";
+
+export default function SalesForm() {
+  const { register, handleSubmit, handleCreation, errors } = useSales();
+
+  return (
+    <form
+      className="flex flex-col items-center gap-4"
+      onSubmit={handleSubmit(handleCreation)}
+    >
+      <Input
+        label="Nome"
+        type="text"
+        {...register("name")}
+        color={`${errors.name ? "danger" : "default"}`}
+        errorMessage={errors.name && `${errors.name.message}`}
+      />
+      <Input
+        label="Preço"
+        type="number"
+        {...register("price")}
+        color={`${errors.price ? "danger" : "default"}`}
+        errorMessage={errors.price && `${errors.price.message}`}
+      />
+      <Input
+        label="Quantidade"
+        type="number"
+        {...register("on_stock")}
+        color={`${errors.on_stock ? "danger" : "default"}`}
+        errorMessage={errors.on_stock && `${errors.on_stock.message}`}
+      />
+      <Button color="primary" radius="full" type="submit">
+        Enviar
+      </Button>
+    </form>
+  );
+}
